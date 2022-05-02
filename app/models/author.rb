1da2eeae7e155703ffd959462e4b26c58ce3ba89
Alexis <amart78@outlook.com>
2022-05-01 21:31:51 -0500

    commit tree afaa14e322d01c45f23f11b338721de7e5edc61e

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ApplicationRecord
+
+#1. All authors have a name
+    validates :name, presence: true
+
+#2. No two authors have the same name
+    validates :name, uniqueness: true
+#3. Author phone numbers are exactly ten digits
+    validates :phone_number,length: {is: 10}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,31 @@
 class Post < ApplicationRecord
+#1. All posts have a title
+    validates :title, presence: true
+
+#2.  Post content is at least 250 characters long
+    validates :content, length: {minimum: 250}
+
+#3. Post summary is a maximum of 250 characters
+    validates :summary, length: {maximum: 250}
+
+#4. Post category is either Fiction or Non-Fiction.  This step requires an inclusion validator, 
+#   which was not outlined in the lesson. You'll need to refer to the Rails guide to look up how to use it.
+    validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+
+#5. Add a custom validator to the Post model that ensures the title is sufficiently clickbait-y. 
+#   The validator should add a validation error if the title does not contain:
+    #   "Won't Believe",  "Secret", "Top [number]", "Guess"
+    validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+
+    def clickbait?
+      if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, 'must be clickbait')
+      end
+    end
+#   Ruby regular expressions are defined between two forward slashes to differentiate them from other language syntax.
+#   adding i makes ruby regex case insensitive
+#   \d is shorthand for [0-9]
+
 end
